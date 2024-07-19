6631c88b53979ff2f1340e925ea19d1a52c0e5b5
esbrinachain <ebrinachain@email.com>
2024-07-19 18:50:43 +0200

    ok

--- src/app/pregunta/pregunta.component.ts
+++ src/app/pregunta/pregunta.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { es, en, cat } from "../idioma";
+import { initializeApp } from "firebase/app";
+import 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, getDocs, doc, setDoc, addDoc, Timestamp } from 'firebase/firestore';
+import { getAuth, createUserWithEmailAndPassword, signOut } from "firebase/auth";
 
 @Component({
   selector: 'app-pregunta',
@@ -8,6 +14,16 @@ import { Component } from '@angular/core';
   styleUrl: './pregunta.component.scss'
 })
 export class PreguntaComponent {
+
+  @Input()
+  idiomaSel: any = es;
+  
+  idiomaSelPreg: any;
+  app: any;
+  db: any;
+  analytics: any;
+  
+
   preg = {
     id_preg: 1,
     anulada: true,
@@ -19,6 +35,35 @@ export class PreguntaComponent {
     fecha_votacion: "26 de julio de 2024",
     recompensa: 10
   };
+
+  constructor() {
+    this.idiomaSelPreg = this.idiomaSel;
+
+
+  }
+
+  
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.idiomaSelPreg = this.idiomaSel;
+    
+    const firebaseConfig = {
+          apiKey: "AIzaSyAHz9zSUk258f3CyoMA2cvE8Kf2BnF442c",
+          authDomain: "esbrinachain-777.firebaseapp.com",
+          projectId: "esbrinachain-777",
+          storageBucket: "esbrinachain-777.appspot.com",
+          messagingSenderId: "825098637790",
+          appId: "1:825098637790:web:1c3930b7e4033004c70d4f",
+          measurementId: "G-Y0VFSVPTBC"
+        };
+        
+    this.app = initializeApp(firebaseConfig);
+    this.db = getFirestore(this.app);
+    this.analytics = getAnalytics(this.app);
+
+  }
 }
 
 // 0xF562C02033DF4b174885D8c7678dC1489340F6d9
